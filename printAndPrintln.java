50c519f902a47c1a4468e0b3e475e0387d8b9b2f
Vivaan Agrawal <agrawal.vivaan@gmail.com>
2026-08-26 21:18:29 -0500

    Added my first project to this repo. It demonstrates the difference between print & println.

---
+++ printAndPrintln.java
@@ -0,0 +1,57 @@
+//Name: Vivaan Agrawal
+//Period: 3A
+public class printAndPrintln
+{
+public static void main(String[] args)
+{
+//2
+System.out.println("Hello World!");
+System.out.println("");
+//3
+System.out.println("Hello");
+System.out.println("World!");
+System.out.println("");
+//4
+System.out.println("Hello");
+System.out.println("");
+System.out.println("World!");
+System.out.println("");
+//5
+System.out.println("*************");
+System.out.println("*** Hello ***");
+System.out.println("*** World ***");
+System.out.println("*************");
+System.out.println("");
+//6
+System.out.print("Hello ");
+System.out.println("World!");
+System.out.println("");
+//7
+System.out.println("###");
+System.out.println("# #");
+System.out.println("###");
+System.out.println("");
+//8
+System.out.print("Vivaan, ");
+System.out.println("Vivaan");
+System.out.println("");
+System.out.println("Vivaan");
+System.out.println("");
+//9
+System.out.print("Something ");
+System.out.print("Something ");
+System.out.println("SOMETHING");
+System.out.println("");
+//10
+System.out.println("Java is");
+System.out.println("cool");
+System.out.println("!");
+System.out.println("");
+//11
+System.out.println("*     *");
+System.out.println(" *   * ");
+System.out.println("  * *  ");
+System.out.println("   *   ");
+System.out.println("");
+}
+}
