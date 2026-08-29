0f2c0a813ce8595434afed45b1cd232231da29db
Vivaan Agrawal <agrawal.vivaan@gmail.com>
2026-08-29 08:57:32 -0500

    Added exercises on variables and datatypes.

--- printAndPrintln.java
+++ printAndPrintln.java
@@ -53,5 +53,26 @@ System.out.println(" *   * ");
 System.out.println("  * *  ");
 System.out.println("   *   ");
 System.out.println("");
+
+//Variables & Data Types ============================>
+//Friday, August 28
+System.out.println("");
+int a = 9;
+int b = 2;
+
+System.out.println(a/b);
+System.out.println(a%b);
+System.out.println(9.0/2);
+
+System.out.println("");
+int myAge = 14;
+double myGPA = 5.5;
+String myName = "Vivaan Agrawal";
+boolean hasPet = false;
+
+System.out.println(myName);
+System.out.println(myAge);
+System.out.println(myGPA);
+System.out.println(hasPet);
 }
 }
